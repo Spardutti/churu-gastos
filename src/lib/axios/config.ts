6aa7e4d0c426e04cdcac48ba6a2bd88bd3fc53c3
Spardutti <luisdamian.sp@gmail.com>
2024-09-20 15:12:58 -0300

    fix(auth headers): clear them when null

--- src/lib/axios/config.ts
+++ src/lib/axios/config.ts
@@ -2,10 +2,14 @@ import axios from 'axios';
 
 export const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-  timeout: 1000,
+  // timeout: 1000,
   // headers: { 'X-Custom-Header': 'foobar' },
 });
 
-export const setDefaultHeaders = (token: string) => {
-  axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+export const setDefaultHeaders = (token: string | null) => {
+  if (!token) {
+    delete axiosInstance.defaults.headers.common['Authorization'];
+  } else {
+    axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+  }
 };
