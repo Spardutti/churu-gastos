126a147979f7e734bb64d6f89a5adb09985882fa
Spardutti <luisdamian.sp@gmail.com>
2024-09-23 07:34:24 -0300

    feat(cards & cards payment): clean unused code

--- src/features/dashboard/components/ExpenseTracker/ExpenseTracker.tsx
+++ src/features/dashboard/components/ExpenseTracker/ExpenseTracker.tsx
@@ -1,13 +1,13 @@
 import Card from '@/components/card';
-import type { IExpense } from '@/features/expenses/types/IExpense';
 import { formatCurrency } from '@/utils/formatCurrency';
 import { useMemo } from 'react';
 
 interface ExpenseTrackerProps {
   budgetLabel: string;
   expensesLabel: string;
-  expenses: IExpense[] | undefined;
+  expenses: number;
   budget: number;
+  extraExpenses?: number;
 }
 
 const styles = {
@@ -15,31 +15,32 @@ const styles = {
   cardText: 'text-white font-bold',
 };
 
-const ExpenseTracker = ({ budgetLabel, expensesLabel, expenses, budget }: ExpenseTrackerProps) => {
-  const monthlyExpense = useMemo(() => {
-    return expenses?.reduce((acc, expense) => {
-      return acc + Number(expense.amount);
-    }, 0);
-  }, [expenses]);
+const ExpenseTracker = ({ budgetLabel, expensesLabel, expenses, budget, extraExpenses }: ExpenseTrackerProps) => {
+  const expenseTotal = useMemo(() => {
+    if (extraExpenses) {
+      return extraExpenses + expenses;
+    }
+    return expenses;
+  }, [expenses, extraExpenses]);
 
   return (
     <div className="flex lg:flex-row flex-col gap-4 lg:gap-10 justify-center">
       <Card>
         <div className={styles.card}>
           <p>{budgetLabel}</p>
-          <p className={styles.cardText}>{formatCurrency({ amount: budget || 0 })}</p>
+          <p className={styles.cardText}>{formatCurrency({ amount: budget })}</p>
         </div>
       </Card>
       <Card>
         <div className={styles.card}>
           <p>{expensesLabel}</p>
-          <p className={styles.cardText}>{formatCurrency({ amount: monthlyExpense! })}</p>
+          <p className={styles.cardText}>{formatCurrency({ amount: expenseTotal })}</p>
         </div>
       </Card>
       <Card>
         <div className={styles.card}>
           <p>Balance </p>
-          <p className={styles.cardText}>{formatCurrency({ amount: budget - monthlyExpense! })}</p>
+          <p className={styles.cardText}>{formatCurrency({ amount: budget - expenses })}</p>
         </div>
       </Card>
     </div>
