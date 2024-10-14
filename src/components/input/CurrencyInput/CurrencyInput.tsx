62d378e402e3b9e45758817217eae53f274e26be
Spardutti <luisdamian.sp@gmail.com>
2024-10-14 17:17:28 -0300

    fix(number input): fix currency input

--- src/components/input/CurrencyInput/CurrencyInput.tsx
+++ src/components/input/CurrencyInput/CurrencyInput.tsx
@@ -1,53 +1,43 @@
-import { formatCurrency } from '@/utils/formatCurrency';
-import React, { useState, useEffect } from 'react';
+import InputLabel from '@/components/input/InputLabel';
+import type { NumberInputProps } from '@mantine/core';
+import { NumberInput } from '@mantine/core';
+import clsx from 'clsx';
 
 interface InputProps {
   label?: string;
-  value: string | number;
-  onChange: (value: string) => void;
+  value: number;
+  onChange: (value: number) => void;
   placeholder?: string;
   min?: number;
   max?: number;
 }
 
-const CurrencyInput: React.FC<InputProps> = ({ label, value, onChange, placeholder, min, max }) => {
-  const [displayValue, setDisplayValue] = useState('');
-
-  useEffect(() => {
-    setDisplayValue(formatInputValue(value));
-  }, [value]);
-
-  const formatInputValue = (val: string | number): string => {
-    const numericValue = typeof val === 'string' ? val.replace(/[^0-9.-]+/g, '') : String(val);
-    const amount = parseFloat(numericValue);
-    return isNaN(amount) ? '' : formatCurrency({ amount });
-  };
-
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const inputValue = e.target.value;
-    const numericValue = inputValue.replace(/[^0-9.-]+/g, '');
-
-    setDisplayValue(formatInputValue(numericValue));
-
-    onChange(numericValue);
-  };
-
-  const handleBlur = () => {
-    setDisplayValue(formatInputValue(value));
+const CurrencyInput = ({ label, value, onChange }: InputProps) => {
+  const handleChange = (e: NumberInputProps) => {
+    if (e.value) {
+      onChange(Number(e.value));
+    }
   };
 
   return (
     <div className="flex gap-1 flex-col">
-      {label && <label className="text-sm font-medium text-gray-700">{label}</label>}
-      <input
-        type="text"
-        value={displayValue}
-        onChange={handleInputChange}
-        onBlur={handleBlur}
-        placeholder={placeholder}
-        min={min}
-        max={max}
-        className="bg-main-background text-main-default-text outline-none focus-within:ring placeholder:text-primary-light border border-black ring-primary-bg px-2 py-1 rounded-md  w-full"
+      {label && <InputLabel text={label} />}
+      <NumberInput
+        allowDecimal
+        allowLeadingZeros={false}
+        allowNegative={false}
+        decimalScale={2}
+        hideControls
+        prefix="$"
+        value={value}
+        placeholder="$0"
+        thousandSeparator
+        onValueChange={handleChange}
+        classNames={{
+          input: clsx(
+            'bg-main-background text-main-default-text outline-none focus-within:ring placeholder:text-primary-light border border-black ring-primary-bg px-2 py-1 rounded-md  w-full',
+          ),
+        }}
       />
     </div>
   );
